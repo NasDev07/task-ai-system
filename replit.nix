08d324a12e2b9d879a963bcda953a5be9beadf3a
NasDev07 <nasruddin20182019@gmail.com>
2026-04-28 16:07:28 +0700

    Fix: Simplify Nix dependencies to use generic package names

--- replit.nix
+++ replit.nix
@@ -1,15 +1,8 @@
 { pkgs }: {
   deps = [
-    pkgs.php80
-    pkgs.php80Extensions.mysql
-    pkgs.php80Extensions.curl
-    pkgs.php80Extensions.openssl
-    pkgs.php80Extensions.mbstring
-    pkgs.php80Extensions.dom
-    pkgs.php80Extensions.xml
-    pkgs.php80Extensions.zip
+    pkgs.php
     pkgs.composer
-    pkgs.nodejs_20
-    pkgs.mysql80
+    pkgs.nodejs
+    pkgs.mysql
   ];
 }
